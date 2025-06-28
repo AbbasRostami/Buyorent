23896ab2f092b4d216810bca85c312565ee686b5
AbbasRostami <abbas.cracker@gmail.com>
2025-06-28 22:00:53 +0330

    feat: update configuration and styles for improved UI
    - Added a new HTTPS configuration for 4kwallpapers.com in next.config.ts to enhance site accessibility.
    - Introduced custom styles for pagination bullets in globals.css to improve user interaction and visual feedback.
    - Updated apexcharts legend styles for better layout consistency.

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "4kwallpapers.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
